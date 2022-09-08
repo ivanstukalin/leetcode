a99a5a4099b8660bbef51bb94794f56d80d36c16
Ivan Stukalin <ivanstukalin@gmail.com>
2022-09-09 01:16:20 +0300

    First task

---
+++ ProductOfArrayExceptSelf.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	result := productExceptSelf(nums)
+	fmt.Printf("Результат:%d", result)
+}
+
+func productExceptSelf(nums []int) []int {
+	var answer []int
+	temp := 1
+	for i := 0; i < len(nums); i++ {
+		answer = append(answer, temp)
+		temp *= nums[i]
+	}
+	temp = 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		answer[i] *= temp
+		temp *= nums[i]
+	}
+	return answer
+}
